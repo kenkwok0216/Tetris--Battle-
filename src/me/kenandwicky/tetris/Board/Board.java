93e449f863cf05aa7e58a2a0b727d378e727a970
kenkwok0216 <74519929+kenkwok0216@users.noreply.github.com>
2021-09-18 21:51:41 +0800

    Update the "data.yml" file to multiplayer

--- src/me/kenandwicky/tetris/Board/Board.java
+++ src/me/kenandwicky/tetris/Board/Board.java
@@ -38,12 +38,17 @@ public class Board {
 		Board.player = player;
 		if (PlayerDirection(player) == "South") {
 			Location blockloc = player.getLocation();
-			settings.getData().set("PlayerPosition.X", (int) blockloc.getX() - 1);
-			settings.getData().set("PlayerPosition.Y", (int) blockloc.getY());
-			settings.getData().set("PlayerPosition.Z", (int) blockloc.getZ());
+			settings.getData().set("Player1Position.X", (int) blockloc.getX() - 1);
+			settings.getData().set("Player1Position.Y", (int) blockloc.getY());
+			settings.getData().set("Player1Position.Z", (int) blockloc.getZ());
+			settings.getData().set("Player2Position.X", (int) blockloc.getX() - 25);
+			settings.getData().set("Player2Position.Y", (int) blockloc.getY());
+			settings.getData().set("Player2Position.Z", (int) blockloc.getZ());			
 			settings.saveData();
 			blockloc = new Location(player.getWorld(),(int) blockloc.getX() - 15,(int) blockloc.getY() - 17,(int) blockloc.getZ() - 7);
-			blockloc.getBlock().setType(Material.STRUCTURE_BLOCK);		
+			blockloc.getBlock().setType(Material.STRUCTURE_BLOCK);
+			blockloc = new Location(player.getWorld(),(int) blockloc.getX() - 33,(int) blockloc.getY(),(int) blockloc.getZ());
+			blockloc.getBlock().setType(Material.STRUCTURE_BLOCK);
 		} else {
 			player.sendMessage("You must facing to South but you are now facing to " + PlayerDirection(player));
 		}		
@@ -52,39 +57,77 @@ public class Board {
 	public static void setup() {
 		if (settings != null) {
 			String worldname = player.getWorld().getName().toString();
-			int x = settings.getData().getInt("PlayerPosition.X");
-			int y = settings.getData().getInt("PlayerPosition.Y");
-			int z = settings.getData().getInt("PlayerPosition.Z");		
+			int x = settings.getData().getInt("Player1Position.X");
+			int y = settings.getData().getInt("Player1Position.Y");
+			int z = settings.getData().getInt("Player1Position.Z");		
 			Bukkit.getWorld(worldname).getBlockAt(x, y, z).setType(Material.AIR);
 			x -= 14; y -= 17; z -= 7;
 			Bukkit.getWorld(worldname).getBlockAt(x, y, z).setType(Material.AIR);
-			x = settings.getData().getInt("PlayerPosition.X") + 16;
-			y = settings.getData().getInt("PlayerPosition.Y") - 17;
-			z = settings.getData().getInt("PlayerPosition.Z") + 32;
-			for (int i = 0; i <= 31; i++) {
+			x -= 33;
+			Bukkit.getWorld(worldname).getBlockAt(x, y, z).setType(Material.AIR);
+			x = settings.getData().getInt("Player1Position.X") + 16;
+			y = settings.getData().getInt("Player1Position.Y") - 17;
+			z = settings.getData().getInt("Player1Position.Z") + 32;
+			for (int i = 0; i <= 64; i++) {
 				for (int j = 0; j <= 25; j++) {
 					if (Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).getType() == Material.RED_CONCRETE) {
-						SavePosition("HoldPosition", x - i, y + j, z);
-						Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						if(i <= 31) {
+							SavePosition("HoldPosition1", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						} else {
+							SavePosition("HoldPosition2", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						}
 					} else if (Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).getType() == Material.ORANGE_CONCRETE) {
-						SavePosition("ScorePosition", x - i, y + j, z);
-						Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						if(i <= 31) {
+							SavePosition("ScorePosition1", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						} else {
+							SavePosition("ScorePosition2", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						}												
 					} else if (Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).getType() == Material.YELLOW_CONCRETE) {
-						SavePosition("LinePosition", x - i, y + j, z);
-						Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						if(i <= 31) {
+							SavePosition("LinePosition1", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						} else {
+							SavePosition("LinePosition2", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						}
 					} else if (Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).getType() == Material.GREEN_CONCRETE) {
-						SavePosition("LevelPosition", x - i, y + j, z);
-						Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						if(i <= 31) {
+							SavePosition("LevelPosition1", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						} else {
+							SavePosition("LevelPosition2", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						}	
 					} else if (Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).getType() == Material.BLUE_CONCRETE) {
-						SavePosition("BoardPosition", x - i, y + j, z);
-						Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						if(i <= 31) {
+							SavePosition("BoardPosition1", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						} else {
+							SavePosition("BoardPosition2", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						}	
 					} else if (Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).getType() == Material.LIGHT_BLUE_CONCRETE) {
-						SavePosition("NextPosition", x - i, y + j, z);
-						Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						if(i <= 31) {
+							SavePosition("NextPosition1", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						} else {
+							SavePosition("NextPosition2", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+						}	
 					} else if (Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).getType() == Material.PURPLE_CONCRETE) {
-						SavePosition("NamePosition", x - i, y + j, z);
-						Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
-						BuildBanner(new Location(player.getWorld(), x - i, y + j, z), '?');
+						if(i <= 31) {
+							SavePosition("NamePosition1", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+							BuildBanner(new Location(player.getWorld(), x - i, y + j, z), '?');
+						} else {
+							SavePosition("NamePosition2", x - i, y + j, z);
+							Bukkit.getWorld(worldname).getBlockAt(x - i, y + j, z).setType(Material.AIR);
+							BuildBanner(new Location(player.getWorld(), x - i, y + j, z), '?');;
+						}	
 					}
 				}
 			}
